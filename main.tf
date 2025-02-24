109236d12db06fa1fdda4a31a11313a066ea2827
Thomas SIDOT <thomas.sidot@etu.mines-ales.fr>
2025-02-24 11:30:13 +0100

    si ça se trouve ça marche

--- main.tf
+++ main.tf
@@ -7,19 +7,40 @@ terraform {
   }
 }
 
+variable "cont_image" {
+  type    = string
+  default = "nginx"
+}
+variable "cont_nb" {
+  type    = number
+  default = 4
+}
+variable "cont_mem" {
+  type    = number
+  default = 512
+}
+variable "cont_priv" {
+  type    = bool
+  default = false
+}
+
+
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+  name         = "${var.cont_image}:latest"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
+  count = var.cont_nb
+  memory = var.cont_mem
   image = docker_image.nginx.image_id
+  privileged = var.cont_priv
   name  = "tutorial"
   ports {
     internal = 80
-    external = 3000+count.index
+    external = sum([3000],[count.index])
   }
 }
 
